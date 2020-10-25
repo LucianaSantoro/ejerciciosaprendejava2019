4c8f02f5a37982552ba31dbbc87f03621c909cbd
Luciana <santoro_luciana@hotmail.com>
2020-10-25 00:03:41 -0300

    Ejercicio siete

---
+++ ejerciciosiente/src/ar/com/ada/online/second/aprendejavaconejercicios2019/Main.java
@@ -0,0 +1,17 @@
+package ar.com.ada.online.second.aprendejavaconejercicios2019;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ejercicio 7: IGual que el programa anterior, pero esta vez la pirámide estará hueca \n" +
+                "(se debe ver únicamente el contorno hecho con asteriscos.)");
+
+        System.out.println("    *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println(" *     *");
+        System.out.println("*********");
+
+    }
+}
