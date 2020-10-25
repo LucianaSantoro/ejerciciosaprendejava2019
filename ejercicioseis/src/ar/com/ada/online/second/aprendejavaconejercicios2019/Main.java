3fe891ef613a5280e82b777d068eea4213c5680f
Luciana <santoro_luciana@hotmail.com>
2020-10-24 23:59:45 -0300

    Ejercicio seis

---
+++ ejercicioseis/src/ar/com/ada/online/second/aprendejavaconejercicios2019/Main.java
@@ -0,0 +1,18 @@
+package ar.com.ada.online.second.aprendejavaconejercicios2019;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ejercicio seis: Escribe un programa que pinte por pantalla una pirámide rellena a base de asteriscos \n" +
+                "La base de la pirámide debe estar formada por 9 asteriscos.");
+
+
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("*********");
+
+
+    }
+}
