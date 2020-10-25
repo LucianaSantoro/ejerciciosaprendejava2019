e18196d00e954e114754e2a71c14b9ee2fedad22
Luciana <santoro_luciana@hotmail.com>
2020-10-24 22:59:12 -0300

    Ejercicio dos

---
+++ ejerciciodos/src/ar/com/ada/online/second/aprendejavaconejercicios2019/Main.java
@@ -0,0 +1,14 @@
+package ar.com.ada.online.second.aprendejavaconejercicios2019;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Ejercicio dos: modifica tu programa anterior para que además muestre tu dirección y tu número" +
+                "de teléfono. Asegúrate de que los datos es muestran en líneas separadas.");
+
+        System.out.println("Nombre: Luciana Santoro. \nDirección: Tu vieja 4282 \nTeléfono: 15-dont-call-me-maybe.");
+
+    }
+}
