2d502510f92bea63d31ebeb23bbce7b5ab30fff3
Luciana <santoro_luciana@hotmail.com>
2020-10-24 23:55:14 -0300

    Ejercicio tres

---
+++ ejerciciotres/src/ar/com/ada/online/second/aprendejavaconejercicios2019/Main.java
@@ -0,0 +1,26 @@
+package ar.com.ada.online.second.aprendejavaconejercicios2019;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Ejercicio tres: escribe un programa que muestre or pantalla 10 palabras en ingles junto \n" +
+                "a su correspondiente traducción al castellano. Las palabras deben estar distribuidas en dos columnas \n" +
+                "y alineadas a la izquierda.");
+
+
+        System.out.println("Español             Inglés");
+        System.out.println("--------------------------");
+        System.out.printf("%-10s            %-10s", "computer", "computadora \t\n");
+        System.out.printf("%-10s            %-10s", "student", "alumne\t\n");
+        System.out.printf("%-10s            %-10s", "cat", "gato\t\n");
+        System.out.printf("%-10s            %-10s", "penguin", "pingüino\t\n");
+        System.out.printf("%-10s            %-10s", "machine", "máquina\t\n");
+        System.out.printf("%-10s            %-10s", "nature", "naturaleza \t\n");
+        System.out.printf("%-10s            %-10s", "light", "luz\t\n");
+        System.out.printf("%-10s            %-10s", "green", "verde\t\n");
+        System.out.printf("%-10s            %-10s", "book", "libro\t\n");
+        System.out.printf("%-10s            %-10s", "pyramid", "pirámide\t\n");
+
+    }
+}
