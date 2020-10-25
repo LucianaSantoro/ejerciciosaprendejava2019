20960dcd564df82082f7841298a93a2248e9a79f
Luciana <santoro_luciana@hotmail.com>
2020-10-24 22:52:50 -0300

    ejercicio uno

---
+++ ejerciciouno/src/ar/com/ada/online/second/aprendejavaconejercicios2019/Main.java
@@ -0,0 +1,12 @@
+package ar.com.ada.online.second.aprendejavaconejercicios2019;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Ejercicio 1: escribe un programa que muestre tu nombre por pantalla.");
+
+        System.out.println("Luciana Santoro.");
+    }
+}
